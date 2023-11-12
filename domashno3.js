336833d13c908b14ab0f05338eb16d26cf2161dc
petrovska4 <tijanapetrovska173@gmail.com>
2023-11-12 13:58:35 +0100

    domashno 3 + timer

--- domashno3.js
+++ domashno3.js
@@ -1,6 +1,7 @@
 var words = ["battery", "school", "register", "coffee", "ready", "black", "visual", "bedroom", "report", "index", "false", "mind", "lunch", "house", "return"];
 var counter = 0;
 var word = new Array();
+var timer = 30;
 
 function Click() {
   var index = Math.floor( Math.random()*15 );
@@ -19,6 +20,27 @@ function Click() {
     else i--;
   }
   document.getElementById("generatedWord").innerHTML += pom;
+
+  var timeCounter = setInterval(function() {
+    time();
+    if( timer < 0 ) {
+      alert("Igrata ne e uspeshna!!!");
+      var button = document.createElement('button');
+      var placeHolder = document.createTextNode("New Game");
+      button.appendChild(placeHolder);
+      button.addEventListener('click', newGame);
+      var div = document.getElementById('div');
+      div.appendChild(button);
+      clearInterval(timeCounter);
+      document.getElementById('timer').innerText = '';
+    } else {
+      timer -= 1;
+    } 
+  },1000);
+}
+
+function time() {
+  timeElement = document.getElementById('timer').innerText = "Imate "+ timer + "s da go pogodite zborot";
 }
 
 function tryThis(e) {
